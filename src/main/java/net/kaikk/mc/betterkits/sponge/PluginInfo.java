abda8b09b16939eaaf5eddfdd9ff495194804e7a
Kai <kai.kikuchi27@gmail.com>
2017-01-03 16:09:04 +0100

    Version 0.9.5

--- src/main/java/net/kaikk/mc/betterkits/sponge/PluginInfo.java
+++ src/main/java/net/kaikk/mc/betterkits/sponge/PluginInfo.java
@@ -3,7 +3,7 @@ package net.kaikk.mc.betterkits.sponge;
 public class PluginInfo {
 	public static final String id = "betterkits";
 	public static final String name = "BetterKits";
-	public static final String version = "0.9.4";
+	public static final String version = "0.9.5";
 	public static final String description = "";
 	
 	private PluginInfo(){}
